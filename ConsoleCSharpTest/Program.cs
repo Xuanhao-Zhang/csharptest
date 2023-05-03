6629a373fb5d1e7a81023e50c82ad473c47eef95
Xuanhao-Zhang <79190330+Xuanhao-Zhang@users.noreply.github.com>
2023-05-03 16:42:14 +0800

    Init C sharp console

---
+++ ConsoleCSharpTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleCSharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test C# Program!");
+        }
+    }
+}
